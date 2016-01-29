2289972dfba7a84e31c59a61c49e9331b5410ea4
David Shriver <dlshriver@cox.net>
2016-01-29 14:08:15 -0600

    new site!

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('site', [])
-	.controller('MainController', function($scope, $http) {
-		$http.get('data/projects.json').success(function(data) {
-			$scope.projects = data;
-			$scope.filteredProjects = $scope.projects;
-		});
-	});
